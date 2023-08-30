651e7416ef4a091eec6126685ff5ab352927688a
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-08-29 19:40:04 -0700

    added tables

--- Models/BlogitemModel.cs
+++ Models/BlogitemModel.cs
@@ -7,6 +7,28 @@ namespace BlogBackEndL.Models
 {
     public class BlogitemModel
     {
-        
+        public int Id {get; set;}
+
+        public int UserId {get; set;}
+
+        public string? Publishername {get; set;}
+
+        public string? Title {get; set;}
+
+        public string? Tag {get; set;}
+
+        public string? Date {get; set;}
+
+        public string? Description {get; set;}
+
+        public string? Image {get; set;}
+
+        public string? Category {get; set;}
+
+        public bool IsDelted {get; set;}
+
+        public bool IsPublished {get; set;}
+
+        public BlogitemModel () {}
     }
 }
